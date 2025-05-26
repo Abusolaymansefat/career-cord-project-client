0b7c1a79614c8279bc44fd9d40ebcb12dd766246
Abusolay <sefat01625@gmail.com>
2025-05-26 09:09:37 +0600

    login page

--- src/Contexts/AuthProvider/AuthProvider.jsx
+++ src/Contexts/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AuthContex } from '../AuthContex/AuthContex';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
+    const [loading, setLoading] = useState(true)
 
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
     const authInfo = {
-
+        loading,
+        createUser
     }
     return (
        <AuthContex value={authInfo}>
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,21 +1,18 @@
-// import Lottie from "lottie-react";
-import Lottie from "lottie-react";
-import React, { use } from "react";
+import React, { use } from 'react';
+import { AuthContex } from '../../Contexts/AuthContex/AuthContex';
+import signinLotte from '../../assets/lottes/sigin.json'
 
-import registerLottie from "../../../assets/lottes/register.json"
-import { AuthContex } from "../../../Contexts/AuthContex/AuthContex";
-
-const Register = () => {
+const SignIn = () => {
 
     const {createUser} = use(AuthContex)
-    const handleRegister = e => {
-        e.preventDefault();
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password)
+        const handleSignIn = e => {
+            e.preventDefault();
+            const form = e.target;
+            const email = form.email.value;
+            const password = form.password.value;
+            console.log(email, password)
 
-        //create user 
+            //create user 
         createUser(email, password)
         .then(result => {
             console.log(result.user)
@@ -23,19 +20,18 @@ const Register = () => {
         .catch(error => {
             console.log(error)
         })
-
-    }
-  return (
-    <div className="hero bg-base-200 min-h-screen">
+        }
+    return (
+        <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           
-          <Lottie style={{width: '200px'}} animationData={registerLottie} loop= {true}></Lottie>
+          <Lottie style={{width: '200px'}} animationData={signinLotte} loop= {true}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <h1 className="text-5xl font-bold">Register now!</h1>
-            <form onSubmit={handleRegister}>
+            <form onSubmit={handleSignIn}>
                 <fieldset className="fieldset">
               <label className="label">Email</label>
               <input type="email" name="email" className="input" placeholder="Email" />
@@ -51,7 +47,7 @@ const Register = () => {
         </div>
       </div>
     </div>
-  );
+    );
 };
 
-export default Register;
+export default SignIn;
