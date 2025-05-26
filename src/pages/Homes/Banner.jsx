518b1c4101bf2d4a977abf974b9775508e3a5953
Abusolay <sefat01625@gmail.com>
2025-05-26 22:51:04 +0600

    motion animation

--- src/pages/Homes/Banner.jsx
+++ src/pages/Homes/Banner.jsx
@@ -1,33 +1,55 @@
 import React from "react";
 import { motion } from "motion/react";
+import team1 from '../../assets/team/teem1.jpg';
+import tram2 from '../../assets/team/team2.jpg';
+import team3 from '../../assets/team/team3.jpg'
 
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <motion.h1
-            animate={{ rotate: 180, transition: { duration: 4 } }}
+        <div className="flex-1">
+          <motion.img
+            src={team1}
+            animate= {{y: [100, 150, 100]}}
+            transition={{duration:5, repeat:Infinity}}
+            className="max-w-sm border-blue-500 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl"
+          />
+          <motion.img
+            src={tram2}
+            animate= {{x: [100, 150, 100]}}
+            transition={{duration:10, delay: 5, repeat:Infinity}}
+            className="max-w-sm border-blue-500 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl"
+          />
+          {/* <motion.img
+            src={team3}
+            animate= {{y: [0, -50, 0]}}
+            transition={{duration:5, repeat:Infinity}}
+            className="max-w-sm border-blue-500 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl"
+          /> */}
+        </div>
+        <div className="flex-1">
+          {/* <motion.h1
+            animate={{ rotate: 180, x:200, y: 200, transition: { duration: 4 } }}
             className="text-5xl font-bold"
           >
             The Easiest Way to Get Your New Job
-          </motion.h1>
+          </motion.h1> */}
           <motion.h1
-            initial= {{scale:0}}
-            animate= {{scale: 1,transition: {duration: 4}}}
+            initial={{ scale: 0 }}
+            animate={{ scale: 1, transition: { duration: 4 } }}
             className="text-5xl font-bold"
           >
-            remot Job The <motion.span
-             animate={
-                {
-                    color: ['#ff5733', '#ffe033', '#9f33ff'],
-                    transition: {duration: 4, repeat: Infinity}
-                }
-                }>Easiest Way</motion.span> to Get Your New Job
+            remot Job The{" "}
+            <motion.span
+              animate={{
+                color: ["#ff5733", "#ffe033", "#9f33ff"],
+                transition: { duration: 4, repeat: Infinity },
+              }}
+            >
+              Easiest Way
+            </motion.span>{" "}
+            to Get Your New Job
           </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
