bcb0b2dc6e887675fe2574841173f0145774d41b
Abusolay <sefat01625@gmail.com>
2025-05-26 22:14:46 +0600

    motion

--- src/pages/Homes/Home.jsx
+++ src/pages/Homes/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            ami Home
+           <Banner></Banner>
         </div>
     );
 };
