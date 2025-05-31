96a022caed60560881695621fb468d107a1ffdf9
Abusolay <sefat01625@gmail.com>
2025-05-31 19:39:30 +0600

    apply

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useParams } from "react-router";
 import UseAuth from "../../hooks/UseAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const JobApply = () => {
   const { id: jobId } = useParams();
@@ -8,30 +10,71 @@ const JobApply = () => {
   const { user } = UseAuth();
   console.log(jobId, user);
 
-  const handleApplyFormSubmit = e => {
+  const handleApplyFormSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    const linkedIn =form.linkedIn.value;
-    const github =form.github.value;
-    const resume =form.resume.value;
+    const linkedIn = form.linkedIn.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
 
-    console.log(linkedIn, github, resume)
+    console.log(linkedIn, github, resume);
 
-  }
+    const application = {
+      jobId,
+      applicant: user.email,
+      linkedIn,
+      github,
+      resume,
+    };
+
+    axios
+      .post("http://localhost:3000/applications", application)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your application has been submitted",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
-      <h2 className="text-4xl">Apply for this job:<Link to={`/jobs/${jobId}`}>details</Link> </h2>
+      <h2 className="text-4xl">
+        Apply for this job:<Link to={`/jobs/${jobId}`}>details</Link>{" "}
+      </h2>
       <form onSubmit={handleApplyFormSubmit}>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-
           <label className="label">LinkedIn Link</label>
-          <input type="url" name="linkedIn" className="input" placeholder="LinkedIn profile Link" />
+          <input
+            type="url"
+            name="linkedIn"
+            className="input"
+            placeholder="LinkedIn profile Link"
+          />
 
           <label className="label">Github Link</label>
-          <input type="url" name="github" className="input" placeholder="Github link" />
+          <input
+            type="url"
+            name="github"
+            className="input"
+            placeholder="Github link"
+          />
 
           <label className="label">Resume Link</label>
-          <input type="usl" name="resume" className="input" placeholder="Resume Link" />
+          <input
+            type="usl"
+            name="resume"
+            className="input"
+            placeholder="Resume Link"
+          />
 
           <input type="submit" className="btn" value="Apply" />
         </fieldset>
