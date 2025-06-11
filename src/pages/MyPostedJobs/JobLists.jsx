2726e9ef2d1fcc0cc2a9c8d8fb9fe8ce11bcc587
Abusolay <sefat01625@gmail.com>
2025-06-11 15:37:43 +0600

    job list

---
+++ src/pages/MyPostedJobs/JobLists.jsx
@@ -0,0 +1,38 @@
+import React, { use } from "react";
+
+const JobLists = ({ jobsCreatedByPromise }) => {
+  const jobs = use(jobsCreatedByPromise);
+  return (
+    <div>
+      <h2 className="text-3xl">jobs created by you: {jobs.length}</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {
+                jobs.map((job, index) => <tr key={job._id}>
+              <th>{index + 1}</th>
+              <td>{job.title}</td>
+              <td>{job.deadline}</td>
+              <td>Blue</td>
+            </tr>)
+            }
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default JobLists;
--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import UseAuth from "../../hooks/UseAuth";
+import JobLists from "./JobLists";
+import { jobsCreatedByPromise } from "../../api/jobsApi";
 
 const MyPostedJobs = () => {
-    return (
-        <div>
-           <h2> My Posted Jobs </h2> 
-        </div>
-    );
+  const { user } = UseAuth();
+  return (
+    <div>
+      <h2> My Posted Jobs: </h2>
+      <Suspense>
+        <JobLists
+          jobsCreatedByPromise={jobsCreatedByPromise(user.email)}
+        ></JobLists>
+      </Suspense>
+    </div>
+  );
 };
 
 export default MyPostedJobs;
